e1d00599b96f7ddefc2bff0018e175b0320b15cb
Nico Straube <git@nico-straube.de>
2022-03-18 20:06:38 +0100

    Get model identifier from serial number
    Previously the model identifier was statically defined in the library, now it is extracted from the base station serial number.

--- src/main/kotlin/de/nicostraube/casacontrol/lib/HttpUtils.kt
+++ src/main/kotlin/de/nicostraube/casacontrol/lib/HttpUtils.kt
@@ -12,13 +12,21 @@ object HttpUtils {
 
     fun getStatusURI(ipAddress: String): URI = URI.create("http://$ipAddress/txcomm.asp")
 
-    private fun getActionURI(ipAddress: String, deviceId: String, commandId: Int, serialNumber: String): URI =
-        URI.create("http://$ipAddress/goform/commtx?command=:02:$deviceId:00:$commandId&serialn=$serialNumber")
+    private fun getActionURI(
+        modelType: String,
+        ipAddress: String,
+        deviceId: String,
+        commandId: Int,
+        serialNumber: String,
+    ): URI =
+        URI.create("http://$ipAddress/goform/commtx?command=:$modelType:$deviceId:00:$commandId&serialn=$serialNumber")
 
 
     fun getActionRequest(deviceData: DeviceData, commandId: Int): HttpRequest =
-        HttpRequest.newBuilder().uri(this.getActionURI(deviceData.baseStation.stationData.ipAddress,
-            deviceData.id,
-            commandId,
-            deviceData.baseStation.stationData.serialNumber)).build()
+        HttpRequest.newBuilder()
+            .uri(this.getActionURI(deviceData.baseStation.stationData.serialNumber.take(2),
+                deviceData.baseStation.stationData.ipAddress,
+                deviceData.id,
+                commandId,
+                deviceData.baseStation.stationData.serialNumber)).build()
 }
